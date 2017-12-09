8e02a9ebb2f6008ffea4c882aa001e9e2270fee1
Jason M. Goncalves <jgoncalves@protonmail.com>
2017-12-09 11:55:42 -0500

    updated variable names, added a message when user hits limit of total amount of guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,26 @@
-// Used to record how many times a letter can be pressed
-let alphabet = [
-		'a','b','c',
-		'd','e','f',
-		'g','h','i',
-		'j','k','l',
-		'm','n','o',
-		'p','q','r',
-		's','t','u',
-		'v','w','x',
-		'y','z'
-	];
+// Used to record how many times a letter can be pressed WITH a space!
+let alphabetWithSpace = [
+    'a', 'b', 'c',
+    'd', 'e', 'f',
+    'g', 'h', 'i',
+    'j', 'k', 'l',
+    'm', 'n', 'o',
+    'p', 'q', 'r',
+    's', 't', 'u',
+    'v', 'w', 'x',
+    'y', 'z', ' '
+];
 //Holds the all the words
-let words =[
-		'hulk',
-		'thor',
-		'iron man',
-		'captain america',
-		'nick fury',
-		'jarvis',
-		'black widow',
-		'loki'
-	];
+let words = [
+    'hulk',
+    'thor',
+    'iron man',
+    'captain america',
+    'nick fury',
+    'jarvis',
+    'black widow',
+    'loki'
+];
 //Holds randomWord
 let randomWord = "";
 //Holds letters in the word
@@ -28,7 +28,7 @@ let letters = [];
 //Holds number of blanks in the word
 let numberOfBlanks = 0;
 //Holds Blanks and correct letter guesses
-let blanks =[];
+let blanks = [];
 //Holds incorrect letter guesses
 let incorrect = [];
 //Counters
@@ -36,150 +36,121 @@ let win = 0;
 let guesses = 9;
 let correct = 0;
 
+function resetGame() { // we will reset when the game is over
+    //Chooses word randombly from the words
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    //Splits the randomWord into individual letters
+    letters = randomWord.split('');
+    //Get the number of blanks
+    numberOfBlanks = letters.length;
 
-function resetGame()
-{
-	//Chooses word randombly from the words
-	randomWord = words[Math.floor(Math.random() * words.length)];
-	//Splits the randomWord into individual letters
-	letters = randomWord.split('');
-	//Get the number of blanks
-	numberOfBlanks = letters.length;
-	
-	// set variables back to default
-	letterGuessed = 0;
-	correct = 0;
-	guesses = 9;
-	incorrect =[];
-	blanks =[];
-	alphabet = [
-		'a','b','c',
-		'd','e','f',
-		'g','h','i',
-		'j','k','l',
-		'm','n','o',
-		'p','q','r',
-		's','t','u',
-		'v','w','x',
-		'y','z'
-	];
-	// test = false;
-	startGame();
-}
-function startGame()
-{
-	//Chooses word randombly from the words
-	randomWord = words[Math.floor(Math.random() * words.length)];
-	//Splits the randomWord into individual letters
-	letters = randomWord.split('');
-	//Get the number of blanks
-	numberOfBlanks = letters.length;
-	
-	// set variables back to default
-	correct = 0;
-	guesses = 9;
-	incorrect =[];
-	blanks =[];
-	alphabet = [
-		'a','b','c',
-		'd','e','f',
-		'g','h','i',
-		'j','k','l',
-		'm','n','o',
-		'p','q','r',
-		's','t','u',
-		'v','w','x',
-		'y','z'
-	];
-
-	//Populate the blanks
-	for(var i = 0; i< numberOfBlanks; i++)
-	{
-		blanks.push('_');
-		document.getElementById('underscores').innerHTML = blanks;
-	}
-
-	// dom manipulation 
-	document.getElementById('underscores').innerHTML = blanks.join(' ');
-	document.getElementById('guesses').innerHTML = guesses;
-	document.getElementById('wins').innerHTML = win;
-	document.getElementById('incorrect').innerHTML = incorrect;
-	// Testing / Debugging
-	// console.log(randomWord);
-	// console.log(letters);
-	// console.log(numberOfBlanks);
-	// console.log(blanks);
+    // set variables back to default
+    letterGuessed = 0;
+    correct = 0;
+    guesses = 9;
+    incorrect = [];
+    blanks = [];
+    alphabetWithSpace = [
+        'a', 'b', 'c',
+        'd', 'e', 'f',
+        'g', 'h', 'i',
+        'j', 'k', 'l',
+        'm', 'n', 'o',
+        'p', 'q', 'r',
+        's', 't', 'u',
+        'v', 'w', 'x',
+        'y', 'z', ' '
+    ];
+    startTheGame = false;
+    startGame(); // start new game
 }
 
-function compareLetters(userKey)
-{
-				console.log('WORKING!');
-				//If guess was correct 
-				if(randomWord.indexOf(userKey) > -1)
-				{
-					//Loops depending on the amount of blanks 
-					for(var i = 0; i < numberOfBlanks; i++)
-					{
-						//Fills in right index with user key
-						if(letters[i] === userKey)
-						{
-							correct++;
-							blanks[i] = userKey;
-							document.getElementById('underscores').innerHTML = blanks.join(' ');
-						}	
-					}
-					//Test / Debug
-					// console.log(blanks);
-				}
-				// if the guess was incorrect
-				else
-				{
-					incorrect.push(userKey);
-					guesses--;
-					// dom manipulation
-					document.getElementById('guesses').innerHTML = guesses;
-					document.getElementById('incorrect').innerHTML = incorrect;
-					//Test / Debug
-					// console.log('Wrong Letters = ' + incorrect);
-					// console.log('Guesses left are ' + guesses);
-				}
-			
-			
-		
-}
-function counter()
-{
-	// When number blanks if filled with right letters then you win
-	if(correct === numberOfBlanks)
-	{
-		//Counts Wins 
-		win++;
-		// dom manipulation
-		document.getElementById('wins').innerHTML = win;
-		alert('You Win');
-		resetGame();
-	}
-	
+function startGame() {
+    //Chooses word randombly from the words
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    //Splits the randomWord into individual letters
+    letters = randomWord.split('');
+    //Get the number of blanks
+    numberOfBlanks = letters.length;
+
+    // set variables back to default
+    correct = 0;
+    guesses = 9;
+    incorrect = [];
+    blanks = [];
+    alphabetWithSpace = [
+        'a', 'b', 'c',
+        'd', 'e', 'f',
+        'g', 'h', 'i',
+        'j', 'k', 'l',
+        'm', 'n', 'o',
+        'p', 'q', 'r',
+        's', 't', 'u',
+        'v', 'w', 'x',
+        'y', 'z', ' '
+    ];
+
+    //Populate the blanks
+    for (let i = 0; i < numberOfBlanks; i++) {
+        blanks.push('_');
+        document.getElementById('underscores').innerHTML = blanks;
+    }
+    // dom manipulation 
+    document.getElementById('underscores').innerHTML = blanks.join(' ');
+    document.getElementById('guesses').innerHTML = guesses;
+    document.getElementById('wins').innerHTML = win;
+    document.getElementById('incorrect').innerHTML = incorrect;
 }
 
-startGame();
-
-document.onkeyup = function(event)
-{
-	// test = true;
-	var letterGuessed = event.key;
-	for(var i = 0; i < alphabet.length; i++)
-	{	
-		if(letterGuessed === alphabet[i] && test === true)
-		{
-			var spliceDword = alphabet.splice(i,1);
-			//Test / Debug
-			// console.log('Double word is = ' + alphabet[i])
-			// console.log('Spliced Word is = ' + spliceDword);
-
-			compareLetters(letterGuessed);
-			counter();
-		}
-	}		
-		
+function compareLetters(event) {
+    //If guess was correct 
+    if (randomWord.indexOf(event) > -1) {
+        //Loops depending on the amount of blanks 
+        for (let i = 0; i < numberOfBlanks; i++) {
+            //Fills in right index with user key
+            if (letters[i] === event) {
+                correct++;
+                blanks[i] = event;
+                document.getElementById('underscores').innerHTML = blanks.join(' ');
+            }
+        }
+    }
+    // if the guess was incorrect
+    else {
+        incorrect.push(event);
+        guesses--;
+        // dom manipulation
+        document.getElementById('guesses').innerHTML = guesses;
+        document.getElementById('incorrect').innerHTML = incorrect;
+    }
 }
+
+function counter() {
+    // When number blanks if filled with right letters then you win
+    if (correct === numberOfBlanks) {
+        // counts the number of wins 
+        win++;
+        // dom manipulation
+        document.getElementById('wins').innerHTML = win;
+        document.getElementById('result').innerHTML = "Congratulations You WIN!";
+        resetGame();
+    } 
+    // when the guesses reach zero, remind the player that they lost and could not save the planet T___T
+    if (guesses === 0) {
+    	// dom manipulation
+    	document.getElementById('result').innerHTML = "Wow, you lost ¯\_(ツ)_/¯";
+    }
+}
+window.onkeyup = function(event) {
+    startTheGame = true;
+    let letterGuessed = event.key;
+    for (let i = 0; i < alphabetWithSpace.length; i++) {
+        if (letterGuessed === alphabetWithSpace[i] && startTheGame === true) {
+            let spliced = alphabetWithSpace.splice(i, 1);
+            compareLetters(letterGuessed);
+            counter();
+        }
+    }
+}
+
+window.onload = startGame(); //start the game on when the windowloads

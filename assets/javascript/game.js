48c1cb7bd9f6dea3ccd982df14c850d90e2789cb
Jason M. Goncalves <jgoncalves@protonmail.com>
2017-12-12 15:14:45 -0500

    removed commented code, cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -67,7 +67,6 @@ function resetGame() { // we will reset when the game is over
         'v', 'w', 'x',
         'y', 'z', ' '
     ];
-    let startTheGame = false; // we want to makesure that the game hasnt started.
     startGame(); // start new game
 }
 
@@ -149,10 +148,9 @@ function counter() {
     }
 }
 window.onkeyup = function(event) {
-    let startTheGame = true; // we want to make sure the game can start 
     let letterGuessed = event.key;
     for (let i = 0; i < alphabetWithSpace.length; i++) {
-        if (letterGuessed === alphabetWithSpace[i] && startTheGame === true) {
+        if (letterGuessed === alphabetWithSpace[i]) {
             let spliced = alphabetWithSpace.splice(i, 1);
             compareLetters(letterGuessed);
             counter();
